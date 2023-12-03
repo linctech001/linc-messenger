fbfe8294876359a33978d454a60b859093ae5767
zhaoyugui <zhaoyugui@gmail.com>
2023-12-03 23:52:59 +0900

    bug fix

--- src/views/layouts/listItem.blade.php
+++ src/views/layouts/listItem.blade.php
@@ -36,7 +36,7 @@ $lastMessageBody = strlen($lastMessageBody) > 30 ? mb_substr($lastMessageBody, 0
     {{-- center side --}}
     <td>
       <p data-id="{{ $user->id }}" data-type="user">
-        {{ strlen($user->name) > 18 ? trim(substr($user->name, 0, 18)) . '..' : $user->name }}
+        {{ mb_strlen($user->name, 'UTF-8') > 18 ? trim(mb_substr($user->name, 0, 18, 'UTF-8')) . '..' : $user->name }}
         <span class="contact-item-time" data-time="{{$lastMessage->created_at}}">{{ $lastMessage->timeAgo }}</span>
       </p>
       <span>
@@ -74,7 +74,7 @@ $lastMessageBody = strlen($lastMessageBody) > 30 ? mb_substr($lastMessageBody, 0
     {{-- center side --}}
     <td>
       <p data-id="{{ $user->id }}" data-type="user">
-        {{ strlen($user->name) > 18 ? trim(substr($user->name, 0, 18)) . '..' : $user->name }}
+      {{ mb_strlen($user->name, 'UTF-8') > 18 ? trim(mb_substr($user->name, 0, 18, 'UTF-8')) . '..' : $user->name }}
     </td>
   </tr>
 </table>
